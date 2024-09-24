5cc30c1092b659264263af80b82da662c0f494f8
Lingbo Liu <piantu@hotmail.com>
2024-09-24 10:07:13 -0400

    update IO

--- knime_extension/src/nodes/geoimageio.py
+++ knime_extension/src/nodes/geoimageio.py
@@ -24,15 +24,28 @@ __NODE_ICON_PATH = "icons/icon/IO/"
     category=__category,
     after="",
 )
+
+@knut.rasterio_node_description(
+    short_description="Reads a GeoTIFF file and outputs the image data, profile, and metadata.",
+    description="This node reads a GeoTIFF image file, outputs the image data, metadata profile as a table, and a serialized image object.",
+    references={
+        "Rasterio Documentation": "https://rasterio.readthedocs.io/",
+        "GeoTIFF Format": "https://gdal.org/drivers/raster/geotiff.html"
+    }
+)
 @knext.output_binary(
     name="Image object",
-    description="Pickle object from raterio package",
+    description="Serialized image data and profile from the GeoTIFF file.",
     id="rasterio.data.profile",
+)
+@knext.output_table(
+    name="Profile table",
+    description="Table of the image profile metadata, including bounds and shape."
 )
 class GeoTiffReaderNode:
     data_url = knext.StringParameter(
         "Input file path",
-        "The file path for reading data.",
+        "The file path for reading the GeoTIFF image.",
         "",
     )
 
@@ -42,19 +55,34 @@ class GeoTiffReaderNode:
 
     def execute(self, exec_context: knext.ExecutionContext):
         exec_context.set_progress(
-            0.4, "Reading file (This might take a while without progress changes)"
+            0.1, "Reading file (This might take a while without progress changes)"
         )
         import rasterio
-
+        import pandas as pd
         dataset = rasterio.open(self.data_url)
+
+        # Read numpy array and profile
         im_data = dataset.read()
         profile = dataset.profile
-        list_output = [im_data, profile]
+        bounds = [*dataset.bounds]  
+        bounds_str = str(bounds)  
+
+        # Profile to table
+        flattened_profile = {k: str(v) for k, v in profile.items()}
+        df_profile = pd.DataFrame(list(flattened_profile.items()), columns=['Property', 'Value'])
+        
+        # Add boundary and shape to Profile table
+        additional_rows = pd.DataFrame([
+            {'Property': 'bounds', 'Value': bounds_str},
+            {'Property': 'shape', 'Value': str(im_data.shape)}
+        ])
+        df_profile = pd.concat([df_profile, additional_rows], ignore_index=True)
+        exec_context.set_progress(0.8, "Profile and metadata extracted...")
+        
         import pickle
+        imagedata = pickle.dumps([im_data, profile])
 
-        imagedata = pickle.dumps(list_output)
-
-        return imagedata
+        return imagedata, knext.Table.from_pandas(df_profile)
 
 
 ############################################
